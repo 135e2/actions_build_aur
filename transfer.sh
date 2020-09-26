8923cc416afdd0830fee75c9d51e6f0078309df6
sinofp <sinofp@tuta.io>
2020-09-26 23:06:32 +0800

    Fix 'Host key verification failed.'

--- transfer.sh
+++ transfer.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 pacman -S rsync openssh --noconfirm --needed &> /dev/null
 echo "$SSH_KEY" > ssh_key
 
-rsync -av --delete -e "ssh -i ./ssh_key -p $SSH_PORT" \
+rsync -av --delete -e "ssh -i ./ssh_key -p $SSH_PORT -o StrictHostKeyChecking=no" \
     /home/builduser/localrepo/ "$REMOTE_REPO"
