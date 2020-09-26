4070d9acf078cfd421fcc5499a9e9e2b390d2f2d
sinofp <sinofp@tuta.io>
2020-09-27 00:04:44 +0800

    Fix unknown public key
    试了各种gpg配置，都不能自动retrieve。算了

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@ set -euxo pipefail
 
 my_pkgs=(
     amazon-corretto-8
-    aurutils
-    colorpicker
-    fcitx-sogoupinyin
-    jetbrains-toolbox
-    miniconda3
+    # aurutils
+    # colorpicker
+    # fcitx-sogoupinyin
+    # jetbrains-toolbox
+    # miniconda3
 )
 
+# Dirty trick to do alias makepkg='makepkg --skippgpcheck'
+sed 's/SKIPPGPCHECK=0/SKIPPGPCHECK=1/' /usr/bin/makepkg -i
 sudo -u builduser aur sync "${my_pkgs[@]}" --no-view --no-confirm --rm-deps
