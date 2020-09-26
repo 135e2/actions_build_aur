b51afe8ef0d81ac423d94358c5c6222131a6a1e8
sinofp <sinofp@tuta.io>
2020-09-26 23:13:32 +0800

    Fix ssh_key permission

--- transfer.sh
+++ transfer.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 pacman -S rsync openssh --noconfirm --needed &> /dev/null
 echo "$SSH_KEY" > ssh_key
+chmod 600 ssh_key
 
-rsync -av --delete -e "ssh -i ./ssh_key -p $SSH_PORT -o StrictHostKeyChecking=no" \
+rsync -av --delete -e "ssh -i ./ssh_key -p $SSH_PORT -q -o StrictHostKeyChecking=no" \
     /home/builduser/localrepo/ "$REMOTE_REPO"
