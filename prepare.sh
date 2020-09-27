98564e24590ca2d5dab7e145add8175c000e001d
sinofp <sinofp@tuta.io>
2020-09-27 11:47:34 +0800

    Use shfmt -i 4 to format bash scripts

--- prepare.sh
+++ prepare.sh
@@ -4,12 +4,12 @@ set -euxo pipefail
 REPO=est
 
 # Update system
-pacman -Syu base-devel --noconfirm --needed &> /dev/null
+pacman -Syu base-devel --noconfirm --needed &>/dev/null
 
 # Create non-root user for makepkg
 useradd builduser -m
 passwd -d builduser
-echo 'builduser ALL=(ALL) ALL' >> /etc/sudoers
+echo 'builduser ALL=(ALL) ALL' >>/etc/sudoers
 
 # Install aurutils
 cp * /home/builduser
@@ -22,7 +22,7 @@ sudo -u builduser makepkg -sci --needed --noconfirm --noprogressbar
 popd
 
 # Create local repository
-cat << EOF >> /etc/pacman.conf
+cat <<EOF >>/etc/pacman.conf
 [$REPO]
 SigLevel = Optional TrustAll
 Server = file:///home/builduser/localrepo
@@ -31,4 +31,4 @@ sudo -u builduser mkdir /home/builduser/localrepo
 sudo -u builduser repo-add "/home/builduser/localrepo/$REPO.db.tar.zst"
 pacman -Sy
 
-cat /home/builduser/makepkg.conf >> /etc/makepkg.conf
+cat /home/builduser/makepkg.conf >>/etc/makepkg.conf
--- transfer.sh
+++ transfer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-pacman -S rsync openssh --noconfirm --needed &> /dev/null
-echo "$SSH_KEY" > ssh_key
+pacman -S rsync openssh --noconfirm --needed &>/dev/null
+echo "$SSH_KEY" >ssh_key
 chmod 600 ssh_key
 
 rsync -av --delete -e "ssh -i ./ssh_key -p $SSH_PORT -q -o StrictHostKeyChecking=no" \
