1397e35a42da381fe978c0907247323222883a81
sinofp <sinofp@tuta.io>
2020-09-29 20:45:39 -0400

    Fix -j quote to avoid job cancel

--- custom/gccemacs.sh
+++ custom/gccemacs.sh
@@ -4,9 +4,12 @@ set -euxo pipefail
 aur fetch -t emacs-native-comp-git
 chmod a+w emacs-native-comp-git
 pushd emacs-native-comp-git
-sed 's/CLANG=/CLANG="YES"/' PKGBUILD -i
+# sed 's/CLANG=/CLANG="YES"/' PKGBUILD -i
 sed 's/LTO=/LTO="YES"/' PKGBUILD -i
 sed 's/M17N=/M17N="YES"/' PKGBUILD -i
+# sed 's/FAST_BOOT=/FAST_BOOT="YES"/' PKGBUILD -i
+sed 's/CFLAGS+=" -g"//' PKGBUILD -i
+sed 's/CXXFLAGS+=" -g"//' PKGBUILD -i
 aurdep=$(aur depends emacs-native-comp-git | grep -v emacs-native-comp-git)
 sudo -u builduser aur sync "$aurdep" --no-view --no-confirm
 sudo -u builduser aur build -- -sci --needed --noconfirm --noprogressbar
