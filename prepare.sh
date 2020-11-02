18bafd1001698e80793859db1211c3ccc31158c8
sinofp <sinofp@tuta.io>
2020-11-02 18:01:34 +0800

    Fix mkdir: cannot create directory ‘/run/user/1000’: Permission denied
    同样的脚本，10.29就跑的好好的，30号直接不能跑了，报这个错
    搜了下，一开始以为是Ubuntu的问题https://github.com/docker/for-linux/issues/1074
    再搜，搜到18年Mint的一个报错https://github.com/keybase/keybase-issues/issues/3254
    相同点除了都是Ubuntu，还有设置了$XDG_RUNTIME_DIR
    
    但我试了试，arch这个docker image里没有设它，
    不管了，直接手动创建得了。

--- prepare.sh
+++ prepare.sh
@@ -11,6 +11,10 @@ useradd builduser -m
 passwd -d builduser
 echo 'builduser ALL=(ALL) ALL' >>/etc/sudoers
 
+# Fix mkdir: cannot create directory ‘/run/user/1000’: Permission denied
+mkdir -p /run/user/1000
+chown -R builduser /run/user/1000
+
 # Install aurutils
 curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/aurutils.tar.gz
 tar -xf aurutils.tar.gz
