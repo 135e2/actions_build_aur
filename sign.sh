422bb19b7cb7eab9d0ac1cacfcbf911b4ed2f127
135e2 <135e2@135e2.tk>
2022-02-13 22:11:48 +0800

    refactor: sign all files built

--- sign.sh
+++ sign.sh
@@ -5,10 +5,20 @@ gpg --keyserver keys.openpgp.org --recv-keys "$GPG_KEY"
 curl -o private_key "$GPG_PRIVATE_KEY_URL"
 gpg --import --pinentry-mode loopback --batch --passphrase "$GPG_PRIVATE_KEY_PASSWORD" private_key
 pushd /home/builduser/localrepo || exit 1
+# sign packages
+# shellcheck disable=SC2086
+for f in $(find $1 -name "*.pkg.tar.zst"); do
+    gpg --detach-sign --pinentry-mode loopback --batch --passphrase "$GPG_PRIVATE_KEY_PASSWORD" "$f"
+    repo-add "$REPO_NAME".db.tar.zst "$f"
+done
+# sign *.{db,files}
 # shellcheck disable=SC2044
 # shellcheck disable=SC2086
-for files in $(find $1 -name "*.pkg.tar.zst"); do
-    repo-add "$REPO_NAME".db.tar.zst "$files"
-    gpg --detach-sign --pinentry-mode loopback --batch --passphrase "$GPG_PRIVATE_KEY_PASSWORD" "$files"
+files=(
+  *.db
+  *.files
+)
+for f in "${files[@]}"; do
+    gpg --detach-sign --pinentry-mode loopback --batch --passphrase "$GPG_PRIVATE_KEY_PASSWORD" "$f"
 done
 popd || exit 1
