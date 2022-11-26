07043ec8c1460fbc22ed624d9fb1a769d0760f1d
Mole Shang <51870710+135e2@users.noreply.github.com>
2022-11-27 00:54:22 +0800

    transfer.sh: change sync path...
    ... since my server got reinstalled.

--- transfer.sh
+++ transfer.sh
@@ -5,4 +5,4 @@ mv "localrepo/135e2.db.tar.zst" "localrepo/135e2.db"
 mv "localrepo/135e2.files.tar.zst" "localrepo/135e2.files"
 pacman -S rclone curl --noconfirm --needed &>/dev/null
 curl -o rclone.conf "$RCLONE_CONFIG_URL"
-rclone sync /home/builduser/localrepo VPS:../www/wwwroot/mirror.135e2.eu.org/archlinux/135e2/x86_64 -P --config rclone.conf
+rclone sync /home/builduser/localrepo VPS:../srv/www/htdocs/mirror/archlinux/135e2/x86_64 -P --config rclone.conf
